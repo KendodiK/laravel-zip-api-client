0ce5e0f9a6b4eeb01e5d964fb89e9515ad826225
LCervus <102804734+KendodiK@users.noreply.github.com>
2026-01-04 21:36:14 +0100

    city modify done

---
+++ resources/views/city/modify.blade.php
@@ -0,0 +1,26 @@
+<table>
+    <tr>
+        <th>Városnév</th>
+        <th>ZIP code</th>
+        <th>Megye Id</th>
+        <th></th>
+    </tr>
+    <tr>
+        <form action="{{ route('cities.store') }}" method="post">
+            @method('POST')
+            @csrf
+            <td>
+                <input type="text" name="name" value="{{$entity['name']}}">
+            </td>
+            <td>
+                <input type="number" name="postalCode" value="{{$entity['postalCode']}}">
+            </td>
+            <td>
+                <input type="hidden" name="countyId" value="{{$entity['countyId']}}">
+            </td>
+            <td>
+                <input type="submit" value="módosítás">
+            </td>
+        </form>
+    </tr>
+</table>
